6addf2ebef3dc10fcf2cfe6c9f4ce66a55aff014
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2026-03-09 14:38:47 +0100

    Revert "Update router.go"
    This reverts commit 4cf3f6c309bb9d3777a8066854e356b37b3fc00b.

--- backend/internal/handler/router.go
+++ backend/internal/handler/router.go
@@ -3,10 +3,10 @@ package handler
 import (
 	"time"
 
-	"github.com/Marco-Pagnanini/App-Unto/backend/internal/domain"
-	"github.com/Marco-Pagnanini/App-Unto/backend/pkg/middleware"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/Marco-Pagnanini/App-Unto/backend/internal/domain"
+	"github.com/Marco-Pagnanini/App-Unto/backend/pkg/middleware"
 )
 
 func SetupRouter(
@@ -19,7 +19,7 @@ func SetupRouter(
 
 	// --- CONFIGURAZIONE CORS ---
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:1420", "http://localhost:8081"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:1420"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
